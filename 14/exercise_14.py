a19e3e45e0ef85b061d11f4007807d0e5a38fc7a
dlettman <derek.lettman@gmail.com>
2021-11-30 12:17:26 -0800

    add initial folder structure and basic helpers

---
+++ 14/exercise_14.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append("..")
+import helpers
+
+def part_one():
+    input = helpers.parse_input("input1.txt")
+    # do stuff here
+    output = input
+    return output
+
+def part_two():
+    input = helpers.parse_input("input2.txt")
+    # do stuff here
+    output = input
+    return output
+
+if __name__ == "__main__":
+    print("*** PART ONE ***")
+    print(part_one())
+    print()
+    print("*** PART TWO ***")
+    print(part_two())
+    print()
---
+++ helpers.py
@@ -0,0 +1,32 @@
+import os
+import pathlib
+import shutil
+
+def parse_input(filename):
+    with open(filename) as file:
+        input_list = file.read().splitlines()
+    return input_list
+
+
+def create_folder_structure():
+    cwd = pathlib.Path().resolve()
+    for i in range(1,26):
+        day_number = str(i).zfill(2)
+        newdir_path = os.path.join(cwd, day_number)
+
+        os.mkdir(newdir_path)
+
+        text_path = os.path.join(newdir_path, "input1.txt")
+        pathlib.Path(text_path).touch()
+
+        text_path = os.path.join(newdir_path, "input2.txt")
+        pathlib.Path(text_path).touch()
+
+        exercise_filename = f"exercise_{day_number}.py"
+        shutil.copy((os.path.join(cwd, "template.py")), os.path.join(newdir_path, exercise_filename))
+
+        shutil.copy((os.path.join(cwd, "__init__.py")), os.path.join(newdir_path, "__init__.py"))
+
+
+if __name__ == "__main__":
+    create_folder_structure()
