323e26bd8a74d975199684be5d14c5bd0e17ffbf
dlettman <derek.lettman@gmail.com>
2021-11-30 13:35:00 -0800

    handle empty data

--- 05/exercise_05.py
+++ 05/exercise_05.py
@@ -4,12 +4,16 @@ import helpers
 
 def part_one(input_filename):
     input = helpers.parse_input(input_filename)
+    if not input:
+        return "*** NO INPUT SUPPLIED ***"
     # do stuff here
     output = input
     return output
 
 def part_two(input_filename):
     input = helpers.parse_input(input_filename)
+    if not input:
+        return "*** NO INPUT SUPPLIED ***"
     # do stuff here
     output = input
     return output
