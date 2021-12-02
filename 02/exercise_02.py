0566ccd8cf791758056fe466d5bccd079ce68659
dlettman <derek.lettman@gmail.com>
2021-12-01 21:07:43 -0800

    solve exercise 02

--- 02/exercise_02.py
+++ 02/exercise_02.py
@@ -2,21 +2,35 @@ import sys
 sys.path.append("..")
 import helpers
 
+
 def part_one(input_filename):
     input_data = helpers.parse_input(input_filename)
-    if not input_data:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input_data
-    return output
+    input_data = [item.split(' ') for item in input_data]
+    horizontal, depth = 0, 0
+    for command, value in input_data:
+        if command == "forward":
+            horizontal += int(value)
+        elif command == "down":
+            depth += int(value)
+        elif command == "up":
+            depth -= int(value)
+    return f"horizontal = {horizontal}, depth = {depth}, product = {horizontal * depth}"
+
 
 def part_two(input_filename):
     input_data = helpers.parse_input(input_filename)
-    if not input_data:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input_data
-    return output
+    input_data = [item.split(' ') for item in input_data]
+    horizontal, depth, aim = 0, 0, 0
+    for command, value in input_data:
+        if command == "forward":
+            horizontal += int(value)
+            depth += int(value) * aim
+        elif command == "down":
+            aim += int(value)
+        elif command == "up":
+            aim -= int(value)
+    return f"horizontal = {horizontal}, depth = {depth}, product = {horizontal * depth}"
+
 
 if __name__ == "__main__":
     print("*** PART ONE ***\n")
@@ -24,4 +38,4 @@ if __name__ == "__main__":
     print(f"REAL RESULT = {part_one('input.txt')}\n\n")
     print("*** PART TWO ***\n")
     print(f"Test result = {part_two('inputtest.txt')}\n")
-    print(f"REAL RESULT = {part_two('input.txt')}\n\n")
+    print(f"REAL RESULT = {part_two('input.txt')}")
