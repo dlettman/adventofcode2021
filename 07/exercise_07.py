43759784efadd328ff1103e66a4ed60efe5c7221
dlettman <derek.lettman@gmail.com>
2021-12-06 21:22:08 -0800

    ugly solve exercise 07

--- 07/exercise_07.py
+++ 07/exercise_07.py
@@ -3,20 +3,45 @@ sys.path.append("..")
 import helpers
 
 def part_one(input_filename):
-    input_data = helpers.parse_input(input_filename)
-    if not input_data:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input_data
-    return output
+    input_data = helpers.parse_input(input_filename)[0].split(",")
+    input_data = [int(item) for item in input_data]
+    best_solution = None
+    for i in range(max(input_data)):
+        total_diff = 0
+        for crab_pos in input_data:
+            fuel_cost = abs(crab_pos - i)
+            total_diff += fuel_cost
+        if not best_solution:
+            best_solution = total_diff
+        if total_diff < best_solution or not best_solution:
+            best_solution = total_diff
+    return best_solution
+
 
 def part_two(input_filename):
-    input_data = helpers.parse_input(input_filename)
-    if not input_data:
-        return "*** NO INPUT SUPPLIED ***"
-    # do stuff here
-    output = input_data
-    return output
+    input_data = helpers.parse_input(input_filename)[0].split(",")
+    input_data = [int(item) for item in input_data]
+    best_solution = None
+
+    dist_map = {}
+    cost_so_far = 0
+    for x in range(max(input_data) + 1):
+        cost_so_far += x
+        dist_map[x] = cost_so_far
+    print(dist_map)
+
+    for i in range(max(input_data)):
+        total_diff = 0
+        for crab_pos in input_data:
+            total_diff += dist_map[abs(i - crab_pos)]
+        if not best_solution:
+            best_solution = total_diff
+        if total_diff < best_solution or not best_solution:
+            best_solution = total_diff
+    return best_solution
+
+
+
 
 if __name__ == "__main__":
     print("*** PART ONE ***\n")
@@ -24,4 +49,4 @@ if __name__ == "__main__":
     print(f"REAL RESULT = {part_one('input.txt')}\n\n")
     print("*** PART TWO ***\n")
     print(f"Test result = {part_two('inputtest.txt')}\n")
-    print(f"REAL RESULT = {part_two('input.txt')}\n\n")
+    print(f"REAL RESULT = {part_two('input.txt')}")
