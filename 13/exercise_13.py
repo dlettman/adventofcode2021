862395eb1303dffa27d8c24cf4587aa54481ffc8
dlettman <derek.lettman@gmail.com>
2021-12-12 21:54:10 -0800

    clean up day 13

--- 13/exercise_13.py
+++ 13/exercise_13.py
@@ -3,8 +3,7 @@ sys.path.append("..")
 import helpers
 
 
-def part_one(input_filename):
-    input_data = helpers.parse_input(input_filename)
+def get_dots_and_folds(input_data):
     dots = set()
     folds = []
     blank_idx = None
@@ -16,78 +15,36 @@ def part_one(input_filename):
             dots.add((int(line.split(",")[0]), int(line.split(",")[1])))
     for line in input_data[(blank_idx + 1):]:
         folds.append([int(line.split("=")[1]), line.split("=")[0][-1]])
-    # print(dots)
+    return dots, folds
+
+
+def fold_paper(dots, fold):
     new_dots = set()
-    # print(folds)
+    fold_coord = int(fold[0])
+    if fold[1] == "x":
+        for dot in dots:
+            x, y = dot[0], dot[1]
+            new_dots.add((x, y) if x <= fold_coord else ((fold_coord - (x - fold_coord)), y))
+    elif fold[1] == "y":
+        for dot in dots:
+            x, y = dot[0], dot[1]
+            new_dots.add((x, y) if y <= fold_coord else (x, (fold_coord - (y - fold_coord))))
+    return new_dots
+
+
+def part_one(input_filename):
+    input_data = helpers.parse_input(input_filename)
+    dots, folds = get_dots_and_folds(input_data)
     for fold in folds[:1]:
-        print(fold)
-        fold_axis_coordinate = int(fold[0])
-        print(f"fold axis co = {fold_axis_coordinate} f1 = {fold[1]}")
-        if fold[1] == "x":
-            print("fold = x")
-            for dot in dots:
-                x, y = dot[0], dot[1]
-                if x > fold_axis_coordinate:
-                    # print("found one")
-                    # print(f"{(fold_axis_coordinate - (fold_axis_coordinate - x), y)}")
-                    new_dots.add((fold_axis_coordinate - (x - fold_axis_coordinate), y))
-                else:
-                    new_dots.add((x, y))
-        elif fold[1] == "y":
-            # print(fold)
-            fold_axis_coordinate = int(fold[0])
-            for dot in dots:
-                x, y = dot[0], dot[1]
-                if y > fold_axis_coordinate:
-                    # print("y greater")
-                    new_dots.add((x, fold_axis_coordinate - (y - fold_axis_coordinate)))
-                else:
-                    # print("y not greater")
-                    new_dots.add((x, y))
-    # print(new_dots)
+        new_dots = fold_paper(dots, fold)
     return len(new_dots)
-        # elif fold[1] == "y":
-        #     for y in range(fold_axis_coordinate):
-        #         pass
 
 
 def part_two(input_filename):
     input_data = helpers.parse_input(input_filename)
-    dots = set()
-    folds = []
-    blank_idx = None
-    for idx, line in enumerate(input_data):
-        if not line:
-            blank_idx = idx
-            break
-        else:
-            dots.add((int(line.split(",")[0]), int(line.split(",")[1])))
-    for line in input_data[(blank_idx + 1):]:
-        folds.append([int(line.split("=")[1]), line.split("=")[0][-1]])
-    # print(dots)
-    # print(folds)
+    dots, folds = get_dots_and_folds(input_data)
     for fold in folds:
-        new_dots = set()
-        print(fold)
-        fold_axis_coordinate = int(fold[0])
-        print(f"fold axis co = {fold_axis_coordinate} f1 = {fold[1]}")
-        if fold[1] == "x":
-            print("fold = x")
-            for dot in dots:
-                x, y = dot[0], dot[1]
-                if x > fold_axis_coordinate:
-                    new_dots.add((fold_axis_coordinate - (x - fold_axis_coordinate), y))
-                else:
-                    new_dots.add((x, y))
-        elif fold[1] == "y":
-            fold_axis_coordinate = int(fold[0])
-            for dot in dots:
-                x, y = dot[0], dot[1]
-                if y > fold_axis_coordinate:
-                    new_dots.add((x, fold_axis_coordinate - (y - fold_axis_coordinate)))
-                else:
-                    new_dots.add((x, y))
-        dots = new_dots.copy()
+        dots = fold_paper(dots, fold)
 
     max_x = max([item[0] for item in dots])
     max_y = max([item[1] for item in dots])
@@ -95,16 +52,16 @@ def part_two(input_filename):
     for y in range(max_y + 1):
         string = ""
         for x in range(max_x + 1):
-            if (x,y) in dots:
-                string += "#"
-            else:
-                string += " "
+            string += ("#" if (x,y) in dots else " ")
         print(string)
+
 
 if __name__ == "__main__":
     print("*** PART ONE ***\n")
     print(f"Test result = {part_one('inputtest.txt')}\n")
     print(f"REAL RESULT = {part_one('input.txt')}\n\n")
     print("*** PART TWO ***\n")
-    print(f"Test result = {part_two('inputtest.txt')}\n")
-    print(f"REAL RESULT = {part_two('input.txt')}\n\n")
+    part_two('inputtest.txt')
+    print(f"Test result = ^^^^^^^^")
+    part_two('input.txt')
+    print(f"REAL RESULT = ^^^^^^^^")
